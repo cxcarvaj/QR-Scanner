202da515333203879e53ab0264611e5b3c6638f4
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-02-06 14:35:43 -0500

    first commit

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Map Page'),
+      ),
+    );
+  }
+}
