bc8b13a0db4bad659c50baaa42cb321fe7dfca3a
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-02-23 12:06:05 -0500

    Implement list of maps UI component

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
           '/map': (_) => MapPage(),
         },
         theme: ThemeData(
+          primaryColor: Colors.deepPurple,
           colorScheme: const ColorScheme.light(
             primary: Colors.deepPurple,
           ),
--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -4,8 +4,15 @@ class MapsPage extends StatelessWidget {
   const MapsPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Maps Page'),
+    return ListView.builder(
+      itemCount: 15,
+      itemBuilder: (_, index) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: const Text('http://google.com'),
+        subtitle: Text('ID: $index'),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        onTap: () {},
+      ),
     );
   }
 }
